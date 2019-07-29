6f2f0507d7aded483fe88dc76933bda4de1935e0
HiroMz <hirotaka.uiji@mizuho-ir.co.jp>
2019-07-29 16:35:21 +0900

    5th commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# https://tutorial.djangogirls.org/ja/django_views/
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
